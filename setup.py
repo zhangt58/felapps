7a3372ded4e354d87315d5a84d9916e869de8149
Tong Zhang <warriorlance@gmail.com>
2015-05-14 13:53:55 +0800

    update gitignore and setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 """FELApps project (felapps)"""
 
 def readme():
-    with open('README.md') as f:
+    with open('README.rst') as f:
         return f.read()
 
 from setuptools import find_packages, setup
 
 appName             = "felapps"
-appVersion          = "1.0.0"
+appVersion          = "1.0.4"
 appDescription      = "High-level applications for FEL commissioning."
 #appLong_description = "High-level applications created for the tunning/commissioning of the free-electron laser facilities."
 appLong_description = readme() + '\n\n'
@@ -33,8 +33,8 @@ setup(name             = appName,
       keywords         = appKeywords,
       packages         = find_packages(exclude=['contrib','tests*']),
       install_requires = requiredpackages,
-      classifiers = ["Programming Language :: Python",
-                     'Topic :: Software Development :: Libraries :: "Python Modules"',
-                     'Topic :: Scientific/Engineering :: Physics :: "FEL High-level Applications"'],
+      classifiers = ['Programming Language :: Python', 
+                     'Topic :: Software Development :: Libraries :: Python Modules', 
+                     'Topic :: Scientific/Engineering :: Physics'],
       )
 
