a7e2d5122956f1f9fd48a6fe8464323a3757cc7c
Tong Zhang <warriorlance@gmail.com>
2015-05-28 11:10:48 +0800

    add functionality of version number control
    version number of apps are now controlled by two files:
    updateVersion.py and versionlist, updateVersion.py reads
    the apps' versions from file versionlist, then modify
    the version control class (AppVersions) in miscutils.py
    module, after that, app versions will update automatically.

---
+++ felapps/utils/miscutils.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# class to control app version number
+class AppVersions():
+    def __init__(self):
+        self.versions = {
+                         'imageviewer': '1.2',
+                         'cornalyzer' : '1.0',
+                         'felformula' : '1.0',
+                         }
+
+    def setVersion(self, verNum, appName = 'imageviewer'):
+        self.versions.update({appName: verNum})
+
+    def getVersion(self, appName = 'imageviewer'):
+        return self.versions.get(appName)
+
