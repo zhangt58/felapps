671e4e6ba11f8d2ee2cfaea565d8fb583aa79e89
Tong Zhang <warriorlance@gmail.com>
2015-05-29 15:35:01 +0800

    rm test, new created tests instead

--- test/test_felapps.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-#import sys
-#sys.path.append('../dist/felapps-1.2.5-py2.py3-none-any.whl')
-
-import felapps
-#felapps.imageviewer.run(maximize = True,logon = False)
-#felapps.felformula.run(maximize = True, logon = False)
-felapps.cornalyzer.run(maximize = True, logon = False)
