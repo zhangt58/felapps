0aacf6a8a7b22c636aee5c2ecc205d9095162598
Tong Zhang <warriorlance@gmail.com>
2015-05-29 15:14:21 +0800

    mv test to tests, use nose to do testing

---
+++ tests/test_felapps.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#import sys
+#sys.path.append('../dist/felapps-1.2.5-py2.py3-none-any.whl')
+
+import felapps
+felapps.imageviewer.run(maximize = True,logon = False)
+felapps.felformula.run(maximize = True, logon = False)
+felapps.cornalyzer.run(maximize = True, logon = False)
