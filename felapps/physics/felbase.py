530c45648a0bf5e502354f9dc4ccafe7f72f1cba
Tong Zhang <warriorlance@gmail.com>
2016-05-19 11:22:58 +0800

    minor update

--- felapps/physics/felbase.py
+++ felapps/physics/felbase.py
@@ -1,13 +1,23 @@
+"""
+Python module for the basis of free-electron laser calculation.
+
+"""
+
 import numpy as np
 import scipy.special as sp
 from scipy.optimize import fsolve
 
-"""
-Python module for the basis of free-electron laser.
-"""
 
-class PhysicalConstants():
-    """physical constants"""
+class PhysicalConstants(object):
+    """Physical constants
+    :param c0: :math:`c_0`, velocity of light in vacuum
+    :param epsilon0: :math:`\epsilon_0`, permittivity in vacuum
+    :param mu0: :math:`\mu_0`, permeability in vacuum
+    :param e0: :math:`e`, electron charge, [C]
+    :param m0: :math:`m_e`, electron mass, [kg]
+    :param h0: :math:`h`, Plank constant
+    :param currentA: :math:`I_A`, Alven current, [A]
+    """
     c0       = 2.99792458E+08
     epsilon0 = 8.854187817620390E-12
     mu0      = np.pi*4E-7
@@ -16,8 +26,9 @@ class PhysicalConstants():
     h0       = 6.62607E-34
     currentA = 17045
 
-class HalbachPerm():
-    def __init__(self, _a = 3.33, _b = -5.47, _c = 1.80, _lambdau=20, _Bu = 1.0):
+
+class HalbachPerm(object):
+    def __init__(self, _a=3.33, _b=-5.47, _c=1.80, _lambdau=20, _Bu=1.0):
         """
         Input parameters:
         _a: first Halbach parameter
@@ -32,26 +43,28 @@ class HalbachPerm():
         self.lambdau = _lambdau
         self.Bu      = _Bu
 
-    def findGap(self, gap0 = 10):
+    def findGap(self, gap0=10):
         """
-        gap0: initial gap value vector, [mm]
-        x: gap, [mm]
-        lambdau: [mm]
+        Solve undulator gap value
+
+        :param gap0: initial gap value vector, [mm]
         """
         fbg = lambda x: self.Bu - self.coef1*np.exp(self.coef2*x/self.lambdau+self.coef3*(x/self.lambdau)**2)
-        return fsolve(fbg, x0 = gap0*np.ones(np.size(self.Bu)))
+        return fsolve(fbg, x0=gap0*np.ones(np.size(self.Bu)))
                     
+
 class FELcalc(PhysicalConstants):
     """
     Analytical calculation for Free-electron Laser physics
+
     Usage: res = FELcalc(p1, p2, p3, p4, p5, p6, p7)
     :param p1: beamEnergy, [MeV]
     :param p2: relative energy spread
-    :param p2: undulator period length, [m]
-    :param p2: avgerage beta function, [m]
-    :param p2: radiation wavelength, [m]
-    :param p2: normalized transverse emittance, [m]
-    :param p2: peak current, [A]
+    :param p3: undulator period length, [m]
+    :param p4: avgerage beta function, [m]
+    :param p5: radiation wavelength, [m]
+    :param p6: normalized transverse emittance, [m]
+    :param p7: peak current, [A]
     :return res: dict, keys: "au", "bu", "gap", "sigmar", "rho1D", "rho3D", "Lg1D", "Lg3D", "Psat", "Pshot", "Pss"
     """
     def __init__(self,
@@ -72,13 +85,14 @@ class FELcalc(PhysicalConstants):
         relativeEnergySpread
         unduPeriodLength [m]
         avgBetaFunc 	 [m]
-	radWavelength 	 [m]
-	normEmittance 	 [m]
-	peakCurrent 	 [A]
+	    radWavelength 	 [m]
+	    normEmittance 	 [m]
+	    peakCurrent 	 [A]
         bunchCharge      [C]
         undulatorLength  [m]
         bunchShape: gaussian or flattop
-	"""
+	    """
+
         self.beamEnergy 	  = _beamEnergy
         self.relativeEnergySpread = _relativeEnergySpread
         self.unduPeriodLength 	  = _unduPeriodLength
@@ -86,10 +100,10 @@ class FELcalc(PhysicalConstants):
 	self.radWavelength 	  = _radWavelength
 	self.normEmittance 	  = _normEmittance
         self.peakCurrent 	  = _peakCurrent
-        self.bunchCharge          = _bunchCharge
-        self.undulatorLength      = _undulatorLength
-        self.bunchShape           = _bunchShape
-        self.undulatorType        = _undulatorType
+        self.bunchCharge      = _bunchCharge
+        self.undulatorLength  = _undulatorLength
+        self.bunchShape       = _bunchShape
+        self.undulatorType    = _undulatorType
 
         if self.bunchShape == 'gaussian':
             self.bunchratio = np.sqrt(2.0*np.pi)
@@ -100,7 +114,7 @@ class FELcalc(PhysicalConstants):
         """
         Apply M. Xie formulae for FEL analytical estimation
         """
-	# Xie Ming Formulae fitted cofs
+	    # Xie Ming Formulae fitted cofs
         a1  = 0.45
         a2  = 0.57
         a3  = 0.55
@@ -135,7 +149,7 @@ class FELcalc(PhysicalConstants):
         
         if self.undulatorType == 'planar':
             b  = au**2.0/2.0/(1+au**2)
-            JJ = sp.jn(0,b) - sp.jn(1,b)
+            JJ = sp.jn(0, b) - sp.jn(1, b)
         else: # helical
             JJ = 1.0
 
@@ -146,12 +160,12 @@ class FELcalc(PhysicalConstants):
         etae = Lg1D/beta*4.0*np.pi*epsilonn/(gamma0*lambdas)
         etag = Lg1D/lambdau*4.0*np.pi*eta
         capLambda =  a1*etad** a2 \
-	      +  a3*etae** a4 \
-	      +  a5*etag** a6 \
-	      +  a7*etae** a8*etag**a9 \
-	      + a10*etad**a11*etag**a12 \
-	      + a13*etad**a14*etae**a15 \
-	      + a16*etad**a17*etae**a18*etag**a19
+                +  a3*etae** a4 \
+                +  a5*etag** a6 \
+	            +  a7*etae** a8*etag**a9 \
+	            + a10*etad**a11*etag**a12 \
+	            + a13*etad**a14*etae**a15 \
+	            + a16*etad**a17*etae**a18*etag**a19
         Lg3D  = Lg1D*(1.0+capLambda)
         rho3D = lambdau/4.0/np.pi/np.sqrt(3)/Lg3D
         Psat  = 1.6*rho1D*(Lg1D/Lg3D)**2.0*gamma0*0.511*Ipk*1.0e6 #W
@@ -165,7 +179,7 @@ class FELcalc(PhysicalConstants):
             self.K  = au
             self.Bu = self.K/0.934/(100*lambdau)
 
-        self.gu     = HalbachPerm(_lambdau = lambdau*1000, _Bu = self.Bu).findGap(gap0=5)
+        self.gu     = HalbachPerm(_lambdau=lambdau*1000, _Bu=self.Bu).findGap(gap0=5)
         self.sigmar = sigmaBeam
         self.rho1D  = rho1D
         self.rho3D  = rho3D
@@ -177,14 +191,14 @@ class FELcalc(PhysicalConstants):
         Lsat        = Lg3D*self.findSatFactor(Nlambda, Lg3D, lambdau)
         Pss         = 1.0/9.0*Pshot*np.exp(Lsat/Lg3D)
         
-        sigmat    = self.bunchCharge/Ipk/self.bunchratio # bunch length (rms) for gaussian, full lenth for rectangle
+        sigmat    = self.bunchCharge/Ipk/self.bunchratio  # bunch length (rms) for gaussian, full lenth for rectangle
         bandwidth = np.sqrt(3.0*np.sqrt(3.0)*rho3D*lambdau/lu)
         
         #Pexit = 1.0/9.0*Pshot*np.exp(Lsat/Lg3D)
 
-        pulseEnergy = self.bunchratio*sigmat*Pss # J
-        photonEnergy = self.h0*self.c0/self.e0/lambdas # eV
-        Np = pulseEnergy/photonEnergy/self.e0 # photon per pulse
+        pulseEnergy = self.bunchratio*sigmat*Pss  # J
+        photonEnergy = self.h0*self.c0/self.e0/lambdas  # eV
+        Np = pulseEnergy/photonEnergy/self.e0  # photon per pulse
 
         return {"01-au"             : self.au, 
                 "02-K"              : self.K,
@@ -206,15 +220,15 @@ class FELcalc(PhysicalConstants):
                 "18-PhotonPerPulse" : Np,
                 }
 
-    def findSatFactor(self, nl, l3, xlamd, factor0 = 20):
-        """
-        factor0: initial saturation factor, saturation length over power gain length
-        nl     : electron count within one unit of FEL wavelength
-        l3     : power gain length (3D)
-        xlamd  : undulator period
+    def findSatFactor(self, nl, l3, xlamd, factor0=20):
+        """ Calculator saturation length in the unit of 3D power gainlength
+        :param factor0: initial saturation factor, saturation length over power gain length
+        :param nl: electron count within one unit of FEL wavelength
+        :param l3: power gain length (3D)
+        :param xlamd: undulator period
         """
         fx = lambda x: 6.0*np.sqrt(3.0*np.pi)*nl*l3*np.sqrt(x) - xlamd*np.exp(x)
-        return fsolve(fx, x0 = factor0*np.ones(np.size(self.Lg3D)))
+        return fsolve(fx, x0=factor0*np.ones(np.size(self.Lg3D)))
 
 def test1():
     inst = FELcalc(17500, 8.5714e-5, 0.08, 15, 1.3e-9, 1.4e-6, 5000, 1.0e-9, 100)
