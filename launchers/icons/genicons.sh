9b8d3e906dc3dc65a602451f822b375bc49f56b8
Tong Zhang <warriorlance@gmail.com>
2016-07-29 10:58:17 +0800

    update icons for felapps

--- launchers/icons/genicons.sh
+++ launchers/icons/genicons.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-px="16 20 22 24 32 40 48 72 96 128 192 256 512"
+# update: 2016-07-28 14:23:44 PM CST
+
+#category="short"
+category="long"
+
+[ -e ${category} ] || mkdir ${category}
+
+px="16 20 22 24 32 40 48 72 96 128 192 256 512 1024"
 for ipx in ${px}
 do
-    [ -e ${ipx} ] || mkdir ${ipx}
-    for appname in {"imageviewer","cornalyzer","felformula","dataworkshop","matchwizard","appdrawer"}
+    [ -e ${category}/${ipx} ] || mkdir ${category}/${ipx}
+    #for appname in {"imageviewer","cornalyzer","felformula","dataworkshop","matchwizard","appdrawer","latticeviewer","wxmpv"}
+    for appname in {"imageviewer","cornalyzer","felformula","dataworkshop","appdrawer","latticeviewer","wxmpv"}
     do
-        convert -resize ${ipx}x${ipx} ./original/${appname}.png .tmp.png
-        mv .tmp.png ${ipx}/${appname}.png
+        convert -resize ${ipx}x${ipx} ./original/${category}/${appname}.png .tmp.png
+        mv .tmp.png ${category}/${ipx}/${appname}.png
         echo "Generate icon with ${ipx}x${ipx} for ${appname}"
     done
 done
