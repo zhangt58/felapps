7a56656197cc9fd59f8d3ff27b77695f7318513f
Tong Zhang <warriorlance@gmail.com>
2016-07-29 11:09:20 +0800

    add 'long' and 'short' icons building script lines

--- felapps/resources/makeicon.sh
+++ felapps/resources/makeicon.sh
@@ -18,10 +18,20 @@ img2py -a -n incrfsicon  increase_font16.png ${respyname}
 img2py -a -n decrfsicon  decrease_font16.png ${respyname}
 
 # make image resources for appdrawer app
-img2py -a -n iicon ../../launchers/icons/128/imageviewer.png  ${respyname}
-img2py -a -n cicon ../../launchers/icons/128/cornalyzer.png   ${respyname}
-img2py -a -n ficon ../../launchers/icons/128/felformula.png   ${respyname}
-img2py -a -n dicon ../../launchers/icons/128/dataworkshop.png ${respyname}
-img2py -a -n micon ../../launchers/icons/128/matchwizard.png  ${respyname}
+#img2py -a -n micon_s ../../launchers/icons/short/128/matchwizard.png   ${respyname}
+img2py -a -n iicon_s ../../launchers/icons/short/128/imageviewer.png   ${respyname}
+img2py -a -n cicon_s ../../launchers/icons/short/128/cornalyzer.png    ${respyname}
+img2py -a -n ficon_s ../../launchers/icons/short/128/felformula.png    ${respyname}
+img2py -a -n dicon_s ../../launchers/icons/short/128/dataworkshop.png  ${respyname}
+img2py -a -n licon_s ../../launchers/icons/short/128/latticeviewer.png ${respyname}
+img2py -a -n wicon_s ../../launchers/icons/short/128/wxmpv.png         ${respyname}
+
+#img2py -a -n micon_l ../../launchers/icons/long/512/matchwizard.png   ${respyname}
+img2py -a -n iicon_l ../../launchers/icons/long/512/imageviewer.png   ${respyname}
+img2py -a -n cicon_l ../../launchers/icons/long/512/cornalyzer.png    ${respyname}
+img2py -a -n ficon_l ../../launchers/icons/long/512/felformula.png    ${respyname}
+img2py -a -n dicon_l ../../launchers/icons/long/512/dataworkshop.png  ${respyname}
+img2py -a -n licon_l ../../launchers/icons/long/512/latticeviewer.png ${respyname}
+img2py -a -n wicon_l ../../launchers/icons/long/512/wxmpv.png         ${respyname}
 
 mv ${respyname} ../utils
