baa8dffe51671cbbee2f3990d4055eaf363b9a1b
Tong Zhang <zhangt@frib.msu.edu>
2017-10-02 14:37:33 -0400

    Add extensions and update requires.

---
+++ scripts/wxmpv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# simple script to call wxmpv app
+# 2016-07-28 03:29:47 PM CST
+#
+
+import felapps
+felapps.wxmpv.run(logon=False, debug=False)
